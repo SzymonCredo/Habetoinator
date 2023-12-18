8e034a076904cb35ffde1d5a2eacb37a6bc3cca0
Szymon <szymon.credo@gmail.com>
2023-12-18 12:32:51 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Habetoinator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Habetoinator;
+
+public partial class MainPage : TabbedPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
